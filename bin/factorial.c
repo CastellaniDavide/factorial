59701878afdcaff7871c0d587fc47837e78c6da9
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-09-08 10:08:39 +0200

    Created bin/template.c

---
+++ bin/factorial.c
@@ -0,0 +1,22 @@
+/**
+ * @file factorial.cpp
+ *
+ * @version 01.01 2020-08-15
+ *
+ * @brief 
+ *
+ * @ingroup factorial
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+#include <stdio.h>
+
+int main() {
+	// printf() displays the string inside quotation
+	printf("factorial");
+	return 0;
+}
